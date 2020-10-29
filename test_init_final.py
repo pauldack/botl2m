9f9ebb67877ae2d6b1ddbd2a422710e206fb9721
pauldack <70517972+pauldack@users.noreply.github.com>
2020-10-29 17:53:23 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -3096,7 +3096,13 @@ class mainCog(commands.Cog):
 		if basicSetting[21] != "1":
 			return await ctx.send('```보이스를 사용하지 않도록 설정되어 있습니다.```', tts=False)
 		return await PlaySound(ctx.voice_client, './sound/오빠.mp3')
-
+	
+	@commands.command(name='!리니지')
+	async def sister_(self, ctx):
+		if basicSetting[21] != "1":
+			return await ctx.send('```보이스를 사용하지 않도록 설정되어 있습니다.```', tts=False)
+		return await PlaySound(ctx.voice_client, './sound/lin.mp3')
+									
 	@commands.command(name='!언니')
 	async def sister_(self, ctx):
 		if basicSetting[21] != "1":
